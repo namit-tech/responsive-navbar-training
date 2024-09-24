14804bf0fa23e74d129be902102b7f5b25165b0f
Namit Jain <Jnamit172@gmail.com>
2024-09-24 23:34:00 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// Toggle menu function
+const menuToggle = document.getElementById('mobile-menu');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
